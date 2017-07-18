c6c550997def35723ea326d9e8c617b6fbc50cf9
EsauOneSystem <esau@1oss.net>
2017-07-18 00:23:03 -0500

    boleteo

--- Netsuite/FE/PERU/FE_TAXIBEAT/LMRY_Boleteo_MasivoSCHDL.js
+++ Netsuite/FE/PERU/FE_TAXIBEAT/LMRY_Boleteo_MasivoSCHDL.js
@@ -9,21 +9,13 @@
 // Recuperamos valores
 var objContext = nlapiGetContext(); 
 var LMRY_script = "LatamReady - PE Boleteo Masivo SCHDL";
-var opcApprobe  = objContext.getSetting('SCRIPT', 'custscript_lmry_law360_stat_acts_app');
-var opcReject   = objContext.getSetting('SCRIPT', 'custscript_lmry_law360_stat_acts_rej');
-var employeeid  = objContext.getSetting('SCRIPT', 'custscript_lmry_law360_stat_acts_emp');
-var createinvo  = objContext.getSetting('SCRIPT', 'custscript_lmry_law360_invoice_create');
 
-// Arreglo para la facturacion
 
-var arrPosic = 0;
 // Empieza el proceso del Schedule
 function BM_main_schedule()
 {
 	try {	
-	 var itemsBus = new Array();	
-		// Log de Errores
-		nlapiLogExecution('ERROR', 'BM_main_schedule, createinvo -> ', 'Proceso Iniciado... - ' + createinvo);		
+	 var itemsBus = new Array();		
 
 		// Ejecuta la busqueda
 		var objResultSearch = nlapiLoadSearch('customrecord_lmry_pe_ei_boleteo_masivo','customsearch_lmry_pe_ei_boleteo_masivo');	
@@ -40,60 +32,86 @@ function BM_main_schedule()
 
 				
 				// Procesa la busqueda consultada
+				var cont=0;
 				var fil = 0;
+				var primerPreimpreso ='';
+				var ultimoPreimpreso ='';
 				while ( fil<objResult.length )
 				{
 					var vali=false;
 					var columnsDetalle	=	objResult[fil].getAllColumns();
 					
 					// Estado de cuenta a Procesar
+					var auxiliar = new Array();
 					var ID = objResult[fil].getValue(columnsDetalle[0]);
-					var serie = objResult[fil].getValue(columnsDetalle[1]);
+					var serie = objResult[fil].getText(columnsDetalle[1]);
 					var preimpreso  = objResult[fil].getValue(columnsDetalle[2]);
 					var subsi 	 = objResult[fil].getValue(columnsDetalle[3]);
 					var cliente  = objResult[fil].getValue(columnsDetalle[4]);
 					var fecha 	 = objResult[fil].getValue(columnsDetalle[5]);
 					var codigo 	 = objResult[fil].getValue(columnsDetalle[7]);
-					var impuesto = objResult[fil].getValue(columnsDetalle[8]);
+					var impuesto = objResult[fil].getText(columnsDetalle[8]);
 					var importe  = objResult[fil].getValue(columnsDetalle[9]);
 					var item 	 = objResult[fil].getValue(columnsDetalle[10]);
+					nlapiLogExecution('ERROR', 'importe', importe);
+					if(fil==0){
+						auxiliar[0]=item;
+						auxiliar[1]=parseFloat(importe);
+						auxiliar[2]=impuesto;
+						auxiliar[3]=codigo;
+						auxiliar[4]=serie;
+						auxiliar[5]=preimpreso;
+						auxiliar[6]=preimpreso;
 
-					for(var i=0;i<itemsBus.length;i++){
-						if(itemsBus[i][4]==serie){
-							vali=true;
-							break;
+						itemsBus[cont]= auxiliar;
+						cont++;
+						primerPreimpreso= preimpreso;
+						nlapiLogExecution('ERROR','1',primerPreimpreso);
+					}else{
+						if(fil==objResult.length-1){
+							ultimoPreimpreso= preimpreso;
+							nlapiLogExecution('ERROR','2',ultimoPreimpreso);
 						}
 
-					}
-					if(vali){
-						itemsBus[i][1]= itemsBus[i][1]+ parseFloat(importe);
 
-					}else{
-						itemsBus[itemsBus.length][0]=item;
-						itemsBus[itemsBus.length][1]=importe;
-						itemsBus[itemsBus.length][2]=impuesto;
-						itemsBus[itemsBus.length][3]=
-						itemsBus[itemsBus.length][4]=
-						itemsBus[itemsBus.length][5]=
-						itemsBus[itemsBus.length][6]=
-					}
-					
+						nlapiLogExecution('ERROR','2.5',itemsBus.length);
 						
+						for(var i=0; i<itemsBus.length; i++){
+							nlapiLogExecution('ERROR','2.6',itemsBus[i][4]);
+
+							if(itemsBus[i][4]==serie){
+
+								vali=true;
+								nlapiLogExecution('ERROR','3',i);
+								break;
+							}
+
+						}
+						if(vali){
+							itemsBus[i][1]= itemsBus[i][1]+ parseFloat(importe);
+							itemsBus[i][6]= preimpreso;
+							nlapiLogExecution('ERROR','4',itemsBus[i][1]);
+							nlapiLogExecution('ERROR','5',itemsBus[i][6]);
+
+						}else{
+							itemsBus[itemsBus.length+1][0]=item;
+							itemsBus[itemsBus.length+1][1]=importe;
+							itemsBus[itemsBus.length+1][2]=impuesto;
+							itemsBus[itemsBus.length+1][3]=codigo;
+							itemsBus[itemsBus.length+1][4]=serie;
+							itemsBus[itemsBus.length+1][5]=preimpreso;
+							itemsBus[itemsBus.length+1][6]=preimpreso;
+
+							nlapiLogExecution('ERROR','ENTRONUEVO','ENTRONUEVO');
+						}
+					}						
 						// Siguiente Registro
 						fil++;
 
 						// Ultima linea
 						if (fil==objResult.length){ break; }
 
-						nlapiLogExecution('ERROR','createinvo',createinvo);
-						// Crea la factura si esta aprobado y activo el feature crear factura
-						if ( pmensaje== 'Aprobado' && (createinvo=='T' || createinvo==true) )
-
-						{
-							var idinvoice = BM_invoice(monto);
-							nlapiLogExecution('ERROR','idinvoice',idinvoice);	
-							
-						}
+						
 						
 				} // Procesa la busqueda consultada
 
@@ -103,29 +121,35 @@ function BM_main_schedule()
 			}
 		} // Ejecuta la busqueda
 		
+		// Crea la factura si esta aprobado y activo el feature crear factura
+		
+			var idinvoice = BM_invoice( subsi, fecha,codigo,serie,primerPreimpreso,ultimoPreimpreso,itemsBus);
+			nlapiLogExecution('ERROR','idinvoice',idinvoice);	
+			
+		
 		// Log de Errores
-		nlapiLogExecution('ERROR', 'SAE_main_schedule -> ', 'Proceso Terminado...');
+		nlapiLogExecution('ERROR', 'BM_main_schedule -> ', 'Proceso Terminado...');
  	}catch(err){		
 		// Envia correo de error al usuario
-		law360_sendemail(err + ' [SAE_main_schedule]', LMRY_script);		
+		//law360_sendemail(err + ' [SBM_main_schedule]', LMRY_script);		
 	}
 }
 
 /* ------------------------------------------------------------------------------------------------------
  * Funcion para crear la factura de un estado de cuenta de tiempos
  * --------------------------------------------------------------------------------------------------- */
-function BM_invoice(montoTot, ArrItem)
+function BM_invoice(Subsi,Fecha,DocuTipo,DocuSeri,PrimerNumero,UltimoNumero, ArrItem)
 {	
 			
-		var invoice_entity  = objContext.getSetting('SCRIPT', 'custscript_lmry_law360_expense_entity');
-		var invoice_item  = objContext.getSetting('SCRIPT', 'custscript_lmry_law360_invoice_item');
-		var invoice_taxc  = objContext.getSetting('SCRIPT', 'custscript_lmry_law360_invoice_taxcode');
+		//var invoice_entity  = objContext.getSetting('SCRIPT', 'custscript_lmry_law360_expense_entity');
+		//var invoice_item  = objContext.getSetting('SCRIPT', 'custscript_lmry_law360_invoice_item');
+		//var invoice_taxc  = objContext.getSetting('SCRIPT', 'custscript_lmry_law360_invoice_taxcode');
 		var idRecord = 0;
 
 		// Valida que este configurado para la generacion de factura
-		if (invoice_item=='' || invoice_item==null || invoice_taxc=='' || invoice_taxc==null){
+		if (Subsi=='' || DocuTipo==null || DocuSeri=='' ){
 			// Log de Errores
-			nlapiLogExecution('ERROR', 'law360_invoice - Falta configurar parametros-> ', invoice_item + ' , ' + invoice_taxc);
+			nlapiLogExecution('ERROR', 'law360_invoice - Falta configurar parametros-> ', DocuSeri + ' , ' + Fecha);
 			
 			// Termina la funcion
 			return idRecord;
@@ -135,17 +159,26 @@ function BM_invoice(montoTot, ArrItem)
 		items=ArrItem;
 
 		/**********************************************
-		 * Crea el nuevo invoice - temporal para luego
+		 * Crea el nuevo invoice 
 		 *********************************************/ 
 		var NewRecord = nlapiCreateRecord('invoice');
-			NewRecord.setFieldValue('subsidiary', );		// Cliente
-			NewRecord.setFieldValue('', );		//Fecha	
+			NewRecord.setFieldValue('subsidiary',Subsi );		// Cliente
+			NewRecord.setFieldValue('', Fecha );		//Fecha	
 			NewRecord.setFieldValue('custbody_lmry_document_type', DocuTipo);		// Latam - Legal Document Type
 			NewRecord.setFieldValue('custbody_lmry_serie_doc_cxc', DocuSeri);		// Latam - Serie CxC
 			NewRecord.setFieldValue('', PrimerNumero);		// Latam - Numero Preimpreso
 			NewRecord.setFieldValue('', UltimoNumero);		// Latam - Numero Final Preimpreso
 
 			for(var i=0; i<items.length; i++){
+
+				nlapiLogExecution('ERROR','item0',items[i][0]);
+				nlapiLogExecution('ERROR','item1',items[i][1]);
+				nlapiLogExecution('ERROR','item2',items[i][2]);
+				nlapiLogExecution('ERROR','item3',items[i][3]);
+				nlapiLogExecution('ERROR','item4',items[i][4]);
+				nlapiLogExecution('ERROR','item5',items[i][5]);
+				nlapiLogExecution('ERROR','item6',items[i][6]);
+
 				/*
 				// Agrega el registro
 				NewRecord.setLineItemValue('item', 'quantity'	, i+1, 1);
@@ -178,255 +211,4 @@ function BM_invoice(montoTot, ArrItem)
 	// Devuel el id nuevo
 	return idRecord;
 	
-}
-/* ------------------------------------------------------------------------------------------------------
- * Funcion que devuelve los contactos del proyecto
- * Busqueda : LatamReady -  Law360 Contact
- * ID 		: customsearch_lmry_law360_contact_email
- * --------------------------------------------------------------------------------------------------- */
-function las360_contact(internalid){
-	var aContact = new Array();
-	// Ejecuta la busqueda  
-	var cResult = nlapiLoadSearch('contact', 'customsearch_lmry_law360_contact_email');	
-		cResult.addFilter(new nlobjSearchFilter('company', null, 'anyof', internalid));
-	var dResult = cResult.runSearch();
-	// Trae rango de 1000 Registros
-	var lResult = dResult.getResults(0, 1000);
-	if (lResult != null && lResult != '')
-	{
-		var intLength = lResult.length;
-
-		// Procesa la busqueda consultada
-		var pos = 0;
-		while ( pos<intLength )	
-		{
-			// Columnas del reporte
-			var datColumns = lResult[pos].getAllColumns();	
-
-			// Coloca los correo en un arreglo
-			aContact[pos] = lResult[pos].getValue(datColumns[2]);
-			// Siguiente registro
-			pos++;
-		}
-	}
-
-	// Libera variables
-	var cResult = null;	
-	var dResult = null;
-	var lResult = null;	
-
-	// devuelve el correo
-	return aContact;
-} 
-
-/* ------------------------------------------------------------------------------------------------------
- * Nota: Envio de mail al usuario.
- * 		 namesubs = ID Subsidiaria
- * 		 nameproj = ID Projecto
- * 		 nameperi = Nombre Periodo
- * --------------------------------------------------------------------------------------------------- */
-function law360_sendcustomer(popcion, projectid, nameperi, strlang)
-{
-	try
-	{
-		// Lista de correos
-		var bcc = new Array();
-		var cco = new Array();
-
-		// Verifica si es One World
-		var featuresubs = objContext.getFeature('SUBSIDIARIES');
-		var employeeid  = objContext.getSetting('SCRIPT', 'custscript_lmry_law360_stat_acts_emp');
-		if (employeeid=='' || employeeid==null) { return false; }
-
-		// Lista de contacto
-		var lContact = las360_contact(projectid);
-		var iContact = lContact.length;
-
-		// Datos del proyecto
-		var record   = nlapiLookupField('job', projectid, ['companyname', 'customer', 'custentity_lmry_law360_email']);
-		var userid   = record.customer;
-		var nameasun = record.companyname;
-		var empfir	 = record.companyname;
-		var empema   = '';
-		// Si no tiene contactos
-		if (iContact==0)
-		{
-			empema   = record.custentity_lmry_law360_email;
-			if (empema=='' || empema==null) {
-				// Correo de preferencias generales
-				empema = nlapiLookupField('employee', employeeid, 'email');
-			}
-		}else{
-			var lin = 0;
-			empema = lContact[lin];
-			lin++
-			while (lin<iContact)
-			{
-				bcc[lin-1] = lContact[lin];
-				// Siguiente contacto
-				lin++;
-			}
-		}
-
-
-		// Verifica si es One World
-		var msg_lg = '';
-		var msg_en = '';
-		var msg_sp = '';
-		if ( featuresubs==true || featuresubs=='T') 
-		{
-			namesubs = nlapiLookupField('customer', userid, 'subsidiary'); 
-			// Campos de la subsidiaria
-			var aFields = new Array();
-				aFields[0] = 'legalname';
-				aFields[1] = 'custrecord_lmry_law360_email_head_logo';
-				aFields[2] = 'custrecord_lmry_law360_email_time_msg_sp';
-				aFields[3] = 'custrecord_lmry_law360_email_time_msg_en';
-			var RecordSub = nlapiLookupField('subsidiary', namesubs, aFields); 
-			// Nombre de la subsidiaria
-			namesubs = RecordSub.legalname;
-			// Logo de la empresa
-			msg_lg = RecordSub.custrecord_lmry_law360_email_head_logo;
-			// Mensaje en espanol
-			msg_sp = RecordSub.custrecord_lmry_law360_email_time_msg_sp;
-			// Mensaje en Ingles
-			msg_en = RecordSub.custrecord_lmry_law360_email_time_msg_en;
-			RecordSub = null;
-		}else{
-			var company = nlapiLoadConfiguration('companyinformation'); 
-			// Nombre de la subsidiaria
-			namesubs = company.getFieldValue('legalname');
-			// Logo de la empresa
-			msg_lg = company.getFieldValue('custrecord_lmry_law360_email_head_logo');
-			// Mensaje en espanol
-			msg_sp = company.getFieldValue('custrecord_lmry_law360_email_time_msg_sp');
-			// Mensaje en Ingles
-			msg_en = company.getFieldValue('custrecord_lmry_law360_email_time_msg_en');
-			company = null;
-		}
-		// Reemplaza variables en el texto en espanol
-		msg_sp = msg_sp.replace('field_customer', empfir);
-		msg_sp = msg_sp.replace('field_company', namesubs);
-		msg_sp = msg_sp.replace('field_job'    , nameasun);
-		msg_sp = msg_sp.replace('field_period' , nameperi);
-		// Reemplaza variables en el texto en ingles
-		msg_en = msg_en.replace('field_customer', empfir);
-		msg_en = msg_en.replace('field_company', namesubs);
-		msg_en = msg_en.replace('field_job'    , nameasun);
-		msg_en = msg_en.replace('field_period' , nameperi);
-
-		// Se optiene el URL
-		var url = '';
-		if ( msg_lg!='' && msg_lg!=null )
-		{
-			var getURL = objContext.getSetting('SCRIPT', 'custscript_lmry_netsuite_location');
-			url = 'https://' + getURL + nlapiLoadFile(msg_lg).getURL();
-		}
-
-		// Generacion txt y envio de email
-		var subject = namesubs 
-			if (strlang.indexOf("sp")>0 || strlang.indexOf("SP")>0)
-			{
-				subject += ' - Estado de Cuenta de Gastos';
-			}else{
-				subject += ' - Expense Statement';
-			}
-		var body =  '<body text="#333333" link="#014684" vlink="#014684" alink="#014684">';
-				body += '<table width="642" border="0" align="center" cellpadding="0" cellspacing="0">';
-					body += '<tr>';
-					body += '<td width="100%" valign="top">';
-						if (msg_lg!='' && msg_lg!=null)
-						{
-							body += '<table width="100%" border="0" align="center" cellpadding="0" cellspacing="0">';
-								body += '<tr>';
-								body += '<td width="100%" colspan="2">';
-								body += '<img src="' + url + '" width="25%" height="25%" border="0"/>'; 
-								body += '</td>';
-								body += '</tr>';
-							body += '</table>';
-						}
-						// Cuerpo del correo
-						body += '<table width="100%" border="0" cellspacing="0" cellpadding="2">';
-							body += '<tr>';
-							body += '<td width="15%">&nbsp;</td>';
-							body += '<td width="70%">';
-							body += '<font style="color:#333333;line-height:200%; font-family:Trebuchet MS, Helvetica, sans-serif; font-size:13px">';
-							// Mensaje personalizado en Espanol e Ingles
-							if (strlang.indexOf("sp")>0 || strlang.indexOf("SP")>0)
-							{
-								body += '<p>Estimado Cliente:</p>';
-								body += '<p>Este es un mensaje automático de "' + namesubs + '".</p>';
-								if (popcion==1) {
-									body += '<p>Gracias por su aprobación al Estado de Cuenta del "' + nameasun;
-									body += '" del período de facturación de "' + nameperi + '".</p>';
-									body += '<p>Se estará emitiendo la factura correspondiente y le estará llegando ';
-									body += 'la confirmación en la brevedad posible.</p>';
-								}else{
-									body += '<p>El Estado de Cuenta del Asunto "' + nameasun;
-									body += '" ha sido rechazado por el cliente.</p>';			
-								}
-							}else{
-								body += '<p>Dear Customer:</p>';
-								body += '<p>This is an automatic message from"' + namesubs + '".</p>';
-								if (popcion==1) {
-									body += '<p>Thank you for your approval of the Statement "' + nameasun;
-									body += '" corresponding to the month of "' + nameperi + '".</p>';
-									body += '<p>The corresponding invoice will be issued and it will be arriving ';
-									body += 'confirmation as soon as possible.</p>';
-								}else{
-									body += '<p>The Statement of "' + nameasun;
-									body += '" has been rejected by the customer.</p>';			
-								}
-							}
-							body += '</font>';
-							body += '</td>';
-							body += '<td width="15%">&nbsp;</td>';
-							body += '</tr>';
-						body += '</table>';
-						body += '<br>';
-						// Mensaje que el correo no se responda
-						body += '<table width="100%" border="0" cellspacing="0" cellpadding="2" bgcolor="#e5e6e7">';
-							body += '<tr>';
-							body += '<td>&nbsp;</td>';
-							body += '</tr>';
-							body += '<tr>';
-							body += '<td width="15%">&nbsp;</td>';
-							body += '<td width="70%" align="center">';
-							body += '<font style="color:#333333;line-height:200%; font-family:Trebuchet MS, Helvetica, sans-serif; font-size:12px;" >';
-							if (strlang.indexOf("sp")>0 || strlang.indexOf("SP")>0)
-							{
-								body += '<i>Este es un mensaje automático. Por favor, no responda este correo electrónico.</i>';
-							}else{
-								body += '<i>This is an automatic message. Please do not reply to this email..</i>';
-							}	
-							body += '</font>';
-							body += '</td>';
-							body += '<td width="15%">&nbsp;</td>';
-							body += '</tr>';
-							body += '<tr>';
-							body += '<td>&nbsp;</td>';
-							body += '</tr>';
-						body += '</table>';
-					body += '</td>';
-					body += '</tr>';
-				body += '</table>';
-			body += '</body>';
-		
-		nlapiLogExecution('ERROR', 'nlapiSendEmail: employeeid, empema, subject' , employeeid + ' , ' + empema + ' , ' + subject);
-
-		// Api de Netsuite para enviar correo electronico
-		nlapiSendEmail(employeeid, empema, subject, body, bcc, cco, null, null);
-
-		// Envio de mail
-		return true;
-	}catch(err){
-		// Log de Errores
-		nlapiLogExecution('ERROR', 'law360_sendapprobe -> ', err);
-
-		// Envia correo de error al usuario
-		law360_sendemail(err + ' [law360_sendapprobe]', LMRY_script);
-
-		// Envio de mail
-		return false;
-	}    
 }
